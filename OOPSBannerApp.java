f625f040ae68835ed1375e9e292b4595714b10e9
Deepti-Jana <deeptij.3690@gmail.com>
2026-03-09 22:29:35 +0530

    UC5 - Array initialization with String.join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,21 @@
 
  /**
  * @author Deepti Jana
- * @version 4.0
+ * @version 5.0
  **/
 public class OOPSBannerApp{
 
     public static void main(String[] args) {
 
         // Create array with 7 banner lines
-        String[] lines = new String[7];
-
-        lines[0] = String.join(" ", "   ***   ", "   ***   ", "******   ", "  *****  ");
-        lines[1] = String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      ");
-        lines[2] = String.join(" ", "**     **", "**     **", "**     **", "**       ");
-        lines[3] = String.join(" ", "**     **", "**     **", "**    ** ", "  *****  ");
-        lines[4] = String.join(" ", "**     **", "**     **", "******   ", "      ** ");
-        lines[5] = String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** ");
-        lines[6] = String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ");
+        String[] lines= {
+        String.join(" ", "   ***   ", "   ***   ", "******   ", "  *****  "),
+        String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "),
+        String.join(" ", "**     **", "**     **", "**     **", "**       "),
+        String.join(" ", "**     **", "**     **", "**    ** ", "  *****  "),
+        String.join(" ", "**     **", "**     **", "******   ", "      ** "),
+        String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** "),
+        String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")};
 
         // Use enhanced for-loop to print
         for (String line : lines) {
