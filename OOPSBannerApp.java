3602be4287d811a06a3ba9ee8e87ff9d6e5f213f
Deepti-Jana <deeptij.3690@gmail.com>
2026-03-09 22:48:53 +0530

    UC6 - Implemented static helper methods for O, P, S patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,25 +1,64 @@
-
- /**
+/**
  * @author Deepti Jana
- * @version 5.0
- **/
-public class OOPSBannerApp{
+ * @version 6.0
+ */
+
+public class OOPSBannerApp {
+
+    // Method to create pattern for letter O
+    public static String[] getOPattern() {
+        return new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        };
+    }
+
+    // Method to create pattern for letter P
+    public static String[] getPPattern() {
+        return new String[]{
+            "**** ",
+            "*   *",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    ",
+            "*    "
+        };
+    }
+
+    // Method to create pattern for letter S
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****",
+            "*    ",
+            "*    ",
+            " *** ",
+            "    *",
+            "    *",
+            "**** "
+        };
+    }
 
     public static void main(String[] args) {
 
-        // Create array with 7 banner lines
-        String[] lines= {
-        String.join(" ", "   ***   ", "   ***   ", "******   ", "  *****  "),
-        String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **      "),
-        String.join(" ", "**     **", "**     **", "**     **", "**       "),
-        String.join(" ", "**     **", "**     **", "**    ** ", "  *****  "),
-        String.join(" ", "**     **", "**     **", "******   ", "      ** "),
-        String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** "),
-        String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  ")};
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        // Use enhanced for-loop to print
-        for (String line : lines) {
-            System.out.println(line); 
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join(" ", o[i], o[i], p[i], s[i]);
         }
-	}
+
+        // Print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
